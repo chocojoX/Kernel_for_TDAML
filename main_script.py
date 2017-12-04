Fixed a bug in the computation of distances in the kernel-derived space

main_script.py
@@ -1,4 +1,6 @@
 from persistence_diagram import *
+from discrete_function import *
+import random
 
 
 def display_2_diagrams(diag1, diag2):
@@ -21,10 +23,26 @@ def display_2_diagrams(diag1, diag2):
 if __name__ == "__main__":
     high = 10
     diag1 = Persistence_diagram(high = high)
-    for i in range(35):
-        y = high * random.random()
-        x = y * random.random()
-        diag1.add_point([x, y])
+    n_points = 5
+    for exp in range(10):
+        for i in range(n_points):
+            y = high * random.random()
+            x = y * random.random()
+            diag1.add_point([x, y])
 
-    diag2 = diag1.create_close_diagram(distance = 0.5)
-    display_2_diagrams(diag1, diag2)
+        kernel_vals = []
+        ds = []
+        distances = [0, 0.1, 0.2, 0.3, 0.4, .5, .6, .7, .9, 1.2, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 6]
+        for d in distances:
+            d = np.abs(d+random.random())
+            ds.append(d)
+            diag2 = diag1.create_close_diagram(distance = d)
+            # display_2_diagrams(diag1, diag2)
+
+            sigma = .2
+            val = 2*np.sqrt(np.pi)* sigma * diag1.kernel_value(diag2, sigma=sigma)
+            norm1 = 2*np.sqrt(np.pi)* sigma * diag1.kernel_value(diag1, sigma=sigma)
+            norm2 = 2*np.sqrt(np.pi)* sigma * diag2.kernel_value(diag2, sigma=sigma)
+            kernel_vals.append(norm1 + norm2 - 2*val)
+        plt.plot(ds, kernel_vals, 'b.')
+    plt.show()

persistence_diagram.py
@@ -53,7 +53,7 @@ class Persistence_diagram(object):
 
 
     def create_close_diagram(self, n_points=10, distance=1, high=None):
-        """ Returns a daigram with at n_points points that is at a distance of exactly distance of the given persistence diagram """
+        """ Returns a daigram with at least n_points points that is at a Wasserstein (infinite) distance of exactly distance of the given persistence diagram """
 
         if high is None:
             high = self.high
@@ -99,3 +99,29 @@ class Persistence_diagram(object):
             new_diagram.add_point([x, y])
 
         return new_diagram
+
+
+    def get_symmetricized_diagram(self):
+        symmetric_diagram = persistence_diagram()
+        for p in self.points:
+            symmetric_diagram.add_point(p)
+            symmetric_diagram.add_point([-p[0], -p[1]])
+
+        return symmetric_diagram
+
+
+    def kernel_value(self, diag2, sigma):
+        val = 0
+        for p1 in self.points:
+            for p2 in diag2.points:
+                x, y = p1
+                x2, y2 = p2
+                val += 1/(8*np.pi*sigma) * ( np.exp(-((x-x2)**2 + (y-y2)**2)/(8*sigma)) -
+                                             np.exp(-((x-y2)**2 + (y-x2)**2)/(8*sigma)) )
+        return val
+
+
+
+    def compute_W1(self, diag2):
+        # TODO
+        pass